e506c3cc46fb6801d4178f0c2915d3863f6ee158
Josh Klodnicki <joshklod@gmail.com>
2022-01-21 14:07:03 -0500

    fix: overlapping login attempts

--- discordInterface.js
+++ discordInterface.js
@@ -18,18 +18,28 @@ class DiscordInterface {
         if (this.connecting)        return this.loginPromise.then(() => this);
         this.connecting = true;
 
-        this.loginPromise = (async () => {
-            console.log('Logging into Discord...');
-            await this.client.login(config.discord.clientToken)
-                .catch(err => {
-                    console.error('Failed to connect to Discord');
-                    throw err;
-                });
-            console.log(`Logged into Discord as ${this.client.user.username}`);
-        })();
+        console.log('Logging into Discord...');
+
+        // client.login() is already an async function, but it resolves
+        // slightly before the 'ready' event.  So in order to reliably use
+        // client.isReady() to determine whether we've connected, we must
+        // maintain the "connecting" state beyond the resolution of
+        // client.login().
+        this.loginPromise = new Promise((resolve, reject) => {
+            this.client.once('ready', resolve);
+            this.client.login(config.discord.clientToken)
+                .catch(reject);
+        });
 
         await this.loginPromise
+            .catch(err => {
+                console.error('Failed to connect to Discord');
+                throw err;
+            })
             .finally(() => { this.connecting = false; });
+
+        console.log(`Logged into Discord as ${this.client.user.username}`);
+
         return this;
     }
 
@@ -43,6 +53,7 @@ class DiscordInterface {
     }
 
     async sendToChannel(channel, text) {
+        await this.login();
         await (await this.getChannel(channel)).send(text);
     }
 }
